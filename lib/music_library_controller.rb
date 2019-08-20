66c1c881a2599da413bf5024dcc4feadcd4b8138
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-20 23:12:24 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -23,7 +23,7 @@ class MusicLibraryController
     end
   end
 
-  def list_songs(idx = nil)
+  def list_songs
     Song.all.sort_by(&:name).each.with_index(1) do |song, idx|
       puts "#{idx}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
