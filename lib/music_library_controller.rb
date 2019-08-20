717f5a1db2893306bfbf405f4c6930eda16e6844
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-20 22:54:31 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -76,7 +76,7 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     song_number = gets.chomp
-    Song.all.sort_by(&:name).each.with_index(1) do |song, idx|
+    Song.all.sort_by(&:name).collect.with_index(1) do |song, idx|
       if song_number == idx
         puts "Playing #{song.name} by #{song.artist.name}"
       end
