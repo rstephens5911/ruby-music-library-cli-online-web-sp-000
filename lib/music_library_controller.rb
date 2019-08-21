38fcec86a31c93f710c6536de7ae0e2706991cc8
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-21 00:20:21 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -67,8 +67,8 @@ class MusicLibraryController
 
   def play_song
     puts "Which song number would you like to play?"
-    song_number.to_i = gets.chomp
-    Song.all[song_number]
+    song_number = gets.chomp
+    Song.all[song_number.to_i]
   end
 
 
